45dc3bad3fa3d01d65eab1549b9748dfc279b0ef
Gozman <thegzmn@gmail.com>
2023-03-04 20:14:54 +0100

    Add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="App">
-      
+      <h1 className="semibold text-5xl text-center font-mona">Wagwan?!</h1>
     </div>
   )
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,36 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  
+  future: {
+    hoverOnlyWhenSupported: true,
   },
+
+  theme: {
+    screens: {
+      xxs: '324px',
+      xs: '356px',
+      sm: '400px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+      '2xl': '1800px',
+    },
+    extend: {
+      colors: {
+        colorName: '#color',
+      },
+      fontFamily: {
+        mona: ['Mona Sans', 'sans-serif'],
+        grotesk: ['Space Grotesk', 'sans-serif'],
+        dm: ['DM Sans', 'sans-serif'],
+        bebas: ['Bebas Neue', 'cursive'],
+      },
+      boxShadow: {
+        shadow: '0px 0px 0px 0px rgba(0, 0, 0, 0.0)',
+      },
+    },
+  },
+
   plugins: [],
 }
