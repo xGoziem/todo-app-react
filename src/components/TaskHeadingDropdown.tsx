9b36374110188b6e28db8f34b4b0a9ca71b2ed76
Gozman <thegzmn@gmail.com>
2023-03-07 20:17:29 +0100

    Update app

--- src/components/TaskHeadingDropdown.tsx
+++ src/components/TaskHeadingDropdown.tsx
@@ -1,8 +1,8 @@
-import { BsPlusCircle, BsPlusLg, BsCheckCircle, BsTrash } from "react-icons/bs";
+import { BsPlusLg, BsCheckCircle, BsTrash } from "react-icons/bs";
 
 const TaskHeadingOptionsDropdown = () => {
   return (
-    <div className="bg-neutral-300 w-56 border-2 border-neutral-400 py-1.5 rounded-xl">
+    <div className="bg-neutral-300 w-56 border-2 border-neutral-400 py-1.5 rounded-xl shadow-xl">
       <div className="flex items-center gap-3 px-4 py-2.5 hover:bg-neutral-400 cursor-pointer">
         <BsPlusLg />
         <p>Add New Task</p>
